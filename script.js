23ecc5a81e1e5c0fdf01d8449f316459fd666403
Dev Jobalia <webdev.jobalia@gmail.com>
2023-05-30 12:54:10 +0530

    game up and running

--- script.js
+++ script.js
@@ -4,41 +4,192 @@ var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
 
-hole.addEventListener('animationiteration', () => {
-     var random = -((Math.random()*300)+150);
-     hole.style.top = random + "px";
-     counter = ++;
+hole.addEventListener("animationiteration", () => {
+  var random = -(Math.random() * 300 + 150);
+  hole.style.top = random + "px";
+  counter++;
 });
-setInterval(function(){
-    var characterTop = 
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if(jumping==0){
-        character.style.top = (characterTop+3)+"px";
-    }
-    var blockLeft = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop);
-    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop||(cTop>holeTop+130)))){
-        alert("Game over. Score: "+(counter-1));
-        character.style.top = 100 + "px";
-        counter = 0;
-    }
-},10);
+setInterval(function () {
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  if (jumping == 0) {
+    character.style.top = characterTop + 3 + "px";
+  }
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+  var cTop = -(500 - characterTop);
+  if (
+    characterTop > 480 ||
+    (blockLeft < 20 &&
+      blockLeft > -50 &&
+      (cTop < holeTop || cTop > holeTop + 130))
+  ) {
+    alert("Game over. Score: " + (counter - 1));
+    character.style.top = 100 + "px";
+    counter = 0;
+  }
+}, 10);
 
-function jump(){
-    jumping = 1;
-    let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
-       var characterTop = 
-       parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-       if((characterTop>6)&&(jumpCount<15)){
-           character.style.top = (characterTop-5)+"px";
-       }
-       if(jumpCount>20){
-           clearInterval(jumpInterval);
-           jumping = 0;
-           jumpCount = 0;
-       }
-       jumpCount++;
-    },10);
+function jump() {
+  jumping = 1;
+  let jumpCount = 0;
+  var jumpInterval = setInterval(function () {
+    var characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    if (characterTop > 6 && jumpCount < 15) {
+      character.style.top = characterTop - 5 + "px";
+    }
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  }, 10);
 }
+
+// OPTION 2
+
+// let move_speed = 3,
+//   grativy = 0.5;
+// let bird = document.querySelector(".bird");
+// let img = document.getElementById("bird-1");
+// let sound_point = new Audio("sounds effect/point.mp3");
+// let sound_die = new Audio("sounds effect/die.mp3");
+
+// // getting bird element properties
+// let bird_props = bird.getBoundingClientRect();
+
+// // This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+// let background = document.querySelector(".background").getBoundingClientRect();
+
+// let score_val = document.querySelector(".score_val");
+// let message = document.querySelector(".message");
+// let score_title = document.querySelector(".score_title");
+
+// let game_state = "Start";
+// img.style.display = "none";
+// message.classList.add("messageStyle");
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.key == "Enter" && game_state != "Play") {
+//     document.querySelectorAll(".pipe_sprite").forEach((e) => {
+//       e.remove();
+//     });
+//     img.style.display = "block";
+//     bird.style.top = "40vh";
+//     game_state = "Play";
+//     message.innerHTML = "";
+//     score_title.innerHTML = "Score : ";
+//     score_val.innerHTML = "0";
+//     message.classList.remove("messageStyle");
+//     play();
+//   }
+// });
+
+// function play() {
+//   function move() {
+//     if (game_state != "Play") return;
+
+//     let pipe_sprite = document.querySelectorAll(".pipe_sprite");
+//     pipe_sprite.forEach((element) => {
+//       let pipe_sprite_props = element.getBoundingClientRect();
+//       bird_props = bird.getBoundingClientRect();
+
+//       if (pipe_sprite_props.right <= 0) {
+//         element.remove();
+//       } else {
+//         if (
+//           bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width &&
+//           bird_props.left + bird_props.width > pipe_sprite_props.left &&
+//           bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height &&
+//           bird_props.top + bird_props.height > pipe_sprite_props.top
+//         ) {
+//           game_state = "End";
+//           message.innerHTML =
+//             "Game Over".fontcolor("red") + "<br>Press Enter To Restart";
+//           message.classList.add("messageStyle");
+//           img.style.display = "none";
+//           sound_die.play();
+//           return;
+//         } else {
+//           if (
+//             pipe_sprite_props.right < bird_props.left &&
+//             pipe_sprite_props.right + move_speed >= bird_props.left &&
+//             element.increase_score == "1"
+//           ) {
+//             score_val.innerHTML = +score_val.innerHTML + 1;
+//             sound_point.play();
+//           }
+//           element.style.left = pipe_sprite_props.left - move_speed + "px";
+//         }
+//       }
+//     });
+//     requestAnimationFrame(move);
+//   }
+//   requestAnimationFrame(move);
+
+//   let bird_dy = 0;
+//   function apply_gravity() {
+//     if (game_state != "Play") return;
+//     bird_dy = bird_dy + grativy;
+//     document.addEventListener("keydown", (e) => {
+//       if (e.key == "ArrowUp" || e.key == " ") {
+//         img.src = "images/Bird-2.png";
+//         bird_dy = -7.6;
+//       }
+//     });
+
+//     document.addEventListener("keyup", (e) => {
+//       if (e.key == "ArrowUp" || e.key == " ") {
+//         img.src = "images/Bird.png";
+//       }
+//     });
+
+//     if (bird_props.top <= 0 || bird_props.bottom >= background.bottom) {
+//       game_state = "End";
+//       message.style.left = "28vw";
+//       window.location.reload();
+//       message.classList.remove("messageStyle");
+//       return;
+//     }
+//     bird.style.top = bird_props.top + bird_dy + "px";
+//     bird_props = bird.getBoundingClientRect();
+//     requestAnimationFrame(apply_gravity);
+//   }
+//   requestAnimationFrame(apply_gravity);
+
+//   let pipe_seperation = 0;
+
+//   let pipe_gap = 35;
+
+//   function create_pipe() {
+//     if (game_state != "Play") return;
+
+//     if (pipe_seperation > 115) {
+//       pipe_seperation = 0;
+
+//       let pipe_posi = Math.floor(Math.random() * 43) + 8;
+//       let pipe_sprite_inv = document.createElement("div");
+//       pipe_sprite_inv.className = "pipe_sprite";
+//       pipe_sprite_inv.style.top = pipe_posi - 70 + "vh";
+//       pipe_sprite_inv.style.left = "100vw";
+
+//       document.body.appendChild(pipe_sprite_inv);
+//       let pipe_sprite = document.createElement("div");
+//       pipe_sprite.className = "pipe_sprite";
+//       pipe_sprite.style.top = pipe_posi + pipe_gap + "vh";
+//       pipe_sprite.style.left = "100vw";
+//       pipe_sprite.increase_score = "1";
+
+//       document.body.appendChild(pipe_sprite);
+//     }
+//     pipe_seperation++;
+//     requestAnimationFrame(create_pipe);
+//   }
+//   requestAnimationFrame(create_pipe);
+// }
